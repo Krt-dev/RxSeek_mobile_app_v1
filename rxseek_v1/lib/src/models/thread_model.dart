916527e5f75bf021cbad4a2a2a4da2e7740c295b
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2024-08-09 03:20:01 +0800

    successfully created thread model

---
+++ rxseek_v1/lib/src/models/thread_model.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Thread {
+  final int threadId;
+  final String threadName;
+  final bool save;
+  final int userId;
+  final Timestamp timeCreated;
+
+  Thread(
+      {required this.threadId,
+      required this.threadName,
+      required this.save,
+      required this.userId,
+      required this.timeCreated});
+
+  factory Thread.fromJson(Map<String, dynamic> json) {
+    return Thread(
+        threadId: json["threadId"],
+        threadName: json["threadName"],
+        save: json["save"],
+        userId: json["userId"],
+        timeCreated: json["timeCreated"]);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "threadId": threadId,
+      "threadName": threadName,
+      "save": save,
+      "userId": userId,
+      "timeCreated": timeCreated
+    };
+  }
+}
