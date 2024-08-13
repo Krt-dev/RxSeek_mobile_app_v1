e8bd29551cc06b67dfc109fc7910df5f34a97d0f
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2024-08-13 16:44:11 +0800

    adjust messageResponseFunction to successfuly get message response using post request

--- rxseek_v1/lib/src/controllers/message_controller.dart
+++ rxseek_v1/lib/src/controllers/message_controller.dart
@@ -35,7 +35,9 @@ class MessageController with ChangeNotifier {
   Future<void> getMessageReponse(Message message) async {
     //mao ni atong post request to process the query sa backend then get the response sa LLM gekan sa backend from the query
     final response = await http.post(
-      Uri.parse("http://localhost:8080/process_query/"),
+      Uri.parse("http://10.0.2.2:8000/process_query/"),
+
+      // Uri.parse("http://127.0.0.1:8000/process_query/"),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -44,26 +46,31 @@ class MessageController with ChangeNotifier {
       }),
     );
 
-    if (response.statusCode == 201) {
-      var jsonResponse = jsonDecode(response.body);
+    try {
+      if (response.statusCode == 200) {
+        var jsonResponse = jsonDecode(response.body);
 
-      Message messageResponse = Message(
-          messageId: DateTime.now().millisecondsSinceEpoch,
-          content: jsonResponse["response"],
-          sender: "system",
-          timeCreated: Timestamp.now());
-
-      try {
-        db
-            .collection("Thread")
-            .doc("sampleThread")
-            .collection("Messages")
-            .add(messageResponse.toJson());
-      } on FirebaseException catch (e) {
-        print({e.toString()});
+        Message messageResponse = Message(
+            messageId: DateTime.now().millisecondsSinceEpoch,
+            content: jsonResponse["response"],
+            sender: "system",
+            timeCreated: Timestamp.now());
+        try {
+          db
+              .collection("Thread")
+              .doc("sampleThread")
+              .collection("Messages")
+              .add(messageResponse.toJson());
+        } on FirebaseException catch (e) {
+          print({e.toString()});
+        }
+        print("succesfful response  ");
+      } else {
+        // throw Exception('Failed to generate response');
+        print('Failed to generate response');
       }
-    } else {
-      throw Exception('Failed to generate response');
+    } catch (e) {
+      print({e});
     }
   }
 
