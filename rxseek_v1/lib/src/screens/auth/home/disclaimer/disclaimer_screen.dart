ac187ec419bfe8e20a61ec6c8661dbbdd16da862
Kurt Sanchez <kurt.sanchez.20@usjr.edu.ph>
2024-09-01 14:30:52 +0800

    QOL phase 2

--- rxseek_v1/lib/src/screens/auth/home/disclaimer/disclaimer_screen.dart
+++ rxseek_v1/lib/src/screens/auth/home/disclaimer/disclaimer_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:rxseek_v1/src/routing/router.dart';
 import 'package:rxseek_v1/src/screens/auth/home/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DisclaimerScreen extends StatelessWidget {
   const DisclaimerScreen({super.key});
@@ -23,68 +24,90 @@ class DisclaimerScreen extends StatelessWidget {
               decoration: const BoxDecoration(
                   borderRadius: BorderRadius.all(Radius.circular(20)),
                   image: DecorationImage(
-                    image: AssetImage("assets/images/Disclaimer picture.png"),
+                    image: AssetImage("assets/images/Disclaimer screen2.png"),
                     fit: BoxFit.cover, // Ensure the image covers the container
                   )),
             ),
           ),
+          // SizedBox(
+          //   height: 134,
+          //   child: Container(
+          //     decoration: const BoxDecoration(
+          //       image: DecorationImage(
+          //           image: AssetImage("assets/images/rxseek_logo_name.png")),
+          //     ),
+          //   ),
+          // ),
+          const SizedBox(height: 50),
           SizedBox(
-            height: 134,
-            child: Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/images/rxseek_logo_name.png")),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-              onPressed: () {
-                showModalBottomSheet<void>(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return SizedBox(
-                      height: 600,
-                      child: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const Padding(
-                              padding: EdgeInsets.all(16.0),
-                              child: Text(
-                                "Disclaimer",
-                                style: TextStyle(
-                                    fontSize: 24, fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            const Padding(
-                              padding: EdgeInsets.all(16.0),
-                              child: Text(
-                                "The information provided by this Language Model (LLM) is for educational and informational purposes only and is not intended as a substitute for diagnosis, or treatment. By using this LLM, you acknowledge that you understand these limitations and agree to consult a qualified healthcare professional for all medical and health-related inquiries. The creators and providers of this LLM are not responsible for any consequences arising from the use or misuse of the information provided. For any questions or concerns about the information provided by this LLM, please contact a licensed healthcare provider.",
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                            ElevatedButton(
-                                onPressed: () {
-                                  GlobalRouter.I.router.go(HomeScreen.route);
-                                },
-                                style: ButtonStyle(
-                                  backgroundColor:
-                                      WidgetStateProperty.all(Colors.blue),
+            width: 400, // Set the desired width
+            height: 70,
+            child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      // ignore: use_full_hex_values_for_flutter_colors
+                      WidgetStateProperty.all(const Color(0xffe37b1b8)),
+                  shape: WidgetStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25))),
+                  elevation: WidgetStateProperty.all(10),
+                ),
+                onPressed: () {
+                  showModalBottomSheet<void>(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return SizedBox(
+                        height: 600,
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.all(16.0),
+                                child: Text(
+                                  "Disclaimer",
+                                  style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold),
                                 ),
-                                child: const Text(
-                                  "Understood",
-                                  style: TextStyle(color: Colors.white),
-                                )),
-                          ],
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.all(16.0),
+                                child: Text(
+                                  "The information provided by this Language Model (LLM) is for educational and informational purposes only and is not intended as a substitute for diagnosis, or treatment. By using this LLM, you acknowledge that you understand these limitations and agree to consult a qualified healthcare professional for all medical and health-related inquiries. The creators and providers of this LLM are not responsible for any consequences arising from the use or misuse of the information provided. For any questions or concerns about the information provided by this LLM, please contact a licensed healthcare provider.",
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              ElevatedButton(
+                                  onPressed: () {
+                                    GlobalRouter.I.router.go(HomeScreen.route);
+                                  },
+                                  style: ButtonStyle(
+                                    backgroundColor:
+                                        WidgetStateProperty.all(Colors.blue),
+                                  ),
+                                  child: const Text(
+                                    "Understood",
+                                    style: TextStyle(color: Colors.white),
+                                  )),
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  },
-                );
-              },
-              child: const Text("Get Started")),
+                      );
+                    },
+                  );
+                },
+                child: Text(
+                  "Get Started",
+                  style: GoogleFonts.quicksand(
+                    textStyle: const TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )),
+          ),
         ],
       ),
     );
