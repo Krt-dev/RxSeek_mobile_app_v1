38b5e408e3f3085381e0479cf40692bd52cb2eaf
Kurt Sanchez <kurt.sanchez.20@usjr.edu.ph>
2025-01-20 15:16:02 +0800

    added typing animation widget

---
+++ rxseek_v1/lib/src/widgets/typing_ani.dart
@@ -0,0 +1,59 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+// First create a new widget for the typing indicator
+class TypingIndicator extends StatefulWidget {
+  const TypingIndicator({super.key});
+
+  @override
+  State<TypingIndicator> createState() => _TypingIndicatorState();
+}
+
+class _TypingIndicatorState extends State<TypingIndicator>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: List.generate(3, (index) {
+          return AnimatedBuilder(
+            animation: _controller,
+            builder: (context, child) {
+              return Container(
+                margin: const EdgeInsets.symmetric(horizontal: 2),
+                height: 8,
+                width: 8,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(
+                    (sin((_controller.value * 2 * pi) + (index * pi / 2)) + 1) /
+                        2,
+                  ),
+                  shape: BoxShape.circle,
+                ),
+              );
+            },
+          );
+        }),
+      ),
+    );
+  }
+}
